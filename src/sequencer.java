66a499c433e3411c5976ec4efcd02ca134c25709
unknown <stunger@MZ026-42.adit.mines.edu>
2015-03-24 09:50:40 -0600

    Added sequence constructor and return method for final result

--- src/sequencer.java
+++ src/sequencer.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
 
-public class sequencer {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class Sequencer {
+	
+	private String sequence;
+	
+	public Sequencer(String x) {
+		sequence = x;
 	}
-
+	
+	public String getSeq() {
+		
+		return sequence;
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter sequence: ");
+		Sequencer s = new Sequencer(sc.nextLine());
+		System.out.println(s.getSeq());
+	}
 }
